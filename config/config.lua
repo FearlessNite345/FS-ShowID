b7e56aab02209dd1f542fff1c05f0959e48231f6
FearlessNite345 <110247392+FearlessNite345@users.noreply.github.com>
2024-08-04 22:58:40 -0400

    Update config.lua

--- config/config.lua
+++ config/config.lua
@@ -1,7 +1,5 @@
 Config = {}
 
-Config.checkForUpdate = true
-
 Config.radius = 5 -- Radius already player you want to show IDs
 Config.duration = 10 -- The amount of time in seconds you want the IDs to be shown
 Config.scale = 0.45 -- The scale of the text above the players head
